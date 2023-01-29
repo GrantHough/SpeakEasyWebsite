a7f6c4590012af1a5b677488b1361eacd0a80816
Grant Hough <grant.l.hough@gmail.com>
2023-01-29 15:14:16 -0800

    testing

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const azureLink = "https://speakeasy.azurewebsites.net/";
+
 $('#submit-btn').on('click', function() {
-    var serverCall = contactServerRephrase($("#text-area").val, 'https://speakeasyextension.herokuapp.com/newsentence').then(response => {
+    var serverCall = contactServerRephrase($("#text-area").val, azureLink + 'newsentence').then(response => {
         console.log(response);
     });
 })
 
 async function sendHttpRequest(method, url, data) {
-    
     var contact = fetch(url, {
         method: method, 
         body: JSON.stringify(data), 
+        mode: 'cors',
         headers: data ? {'Content-Type': 'application/json'} : {}
      }).then(async (response) => {
         return (await response.json());
@@ -18,15 +20,12 @@ async function sendHttpRequest(method, url, data) {
 };
 
 async function contactServerRephrase(msg, url) {
-
-    console.log('background.js: ran contactServerRephrase()')
-    var request = sendHttpRequest('POST', url, { 
-        text: msg, 
-        threshold: $("#tolerance-slider").val, 
-    }).then(async (responseData) => {
-        var dog = await responseData;
-        return dog;
-    });
-    return await request;
-
+        var request = sendHttpRequest('POST', url, { 
+            text: msg,
+            threshold: 1, 
+        }).then(async (responseData) => {
+            var dog = await responseData;
+            return dog;
+        });
+        return await request;
 }
