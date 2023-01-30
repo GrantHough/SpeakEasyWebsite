294bf3447677c0fbccaae83a183966bff043125a
Grant Hough <grant.l.hough@gmail.com>
2023-01-29 17:41:35 -0800

    rahhhh

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const azureLink = "https://speakeasy.azurewebsites.net/";
 
 $('#submit-btn').on('click', function() {
-    console.log($("#text-area").val())
-    console.log('hi');
     var serverCall = contactServerRephrase($("#text-area").val(), azureLink + 'newsentence').then(response => {
         console.log(response);
     });
