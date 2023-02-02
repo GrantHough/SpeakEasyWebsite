efa296469529529ee7ac0ba6d83aef5adaab983a
Grant Hough <grant.l.hough@gmail.com>
2023-02-02 14:09:21 -0800

    asdfasdfasdf

---
+++ index.js
@@ -0,0 +1,5 @@
+$(".main-header-pages-item").on("click", function() {
+
+    alert("Despite everything George Wilson faces, many argue that he was not actually a great man because of how he held his wife captive. “‘I’ve got my wife locked up in there,’ explained Wilson calmly” (136). However, this action was justified, since George knew that his wife had been cheating on him and he was worried that she would continue if he did not stop her. Throughout his life, all George does is respect his wife and care for her, however, she is actively cheating with George, resulting in George taking actions to protect her. In addition, Nick describes that while this is all going on, “the giant eyes of Doctor T. J. Eckleburg kept their vigil” (124). This signifies that there was always someone watching and that people will always get what is coming for them. This is a touch of foreshadowing, which informs readers that something bad will happen Myrtle. ")
+
+});
