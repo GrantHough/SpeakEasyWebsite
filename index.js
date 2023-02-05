e4a78328ab3e7f3b54af4d081e1c05ae5c6cb9ea
Grant Hough <grant.l.hough@gmail.com>
2023-02-04 23:59:16 -0800

    testing logic for trigger function and calling server if trigger successful

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const azureLink = "https://speakeasy.azurewebsites.net/";
-var lastTrigger = 0;
-
+const azureLink = "https://speakeasy.azurewebsites.net/"; //base link for azure paas server
+var lastTrigger = 0; //index for keeping track of how many cycles of n milliseconds it has been since last trigger of server call
+var lastText = ""; //most recent text for the edtior box before new trigger
+var triggerThreshold = 5; //threshold for how many times the n millisecond loop must continue without any text change before a server call
+var loopMS = 4000; //milliseconds before loop recurses
+ 
 //prevent text in the editor box from being too long
 $("#homepage-editor").on("input keypress paste", function(event) {  
     if ($(this).html().length > 420) {
@@ -18,13 +21,33 @@ function timer(amount) {
     }, amount);
 }
 
-timer(4000);
+//starting recursive loop for n milliseconds
+timer(loopSeconds);
 
 //listener for message every n milliseconds
 $("#homepage-editor").on('message', function() {
     //get rid of excessive spaces, might be erroneous, check this out later
-    text = $(this).html().replace("   ", "")
-    console.log(text);
+    text = $(this).html().replaceAll("   ", "")
+    text = text.replaceAll("\n", "")
+    
+    // if the text as changed or it has been triggerThreshold * n milliseconds
+    if (text != lastText || lastTrigger > triggerThreshold) {
+        //setting new value for lastText and resetting last trigger index
+        lastText = text;
+        lastTrigger = 0;
+
+        var serverCall = contactServerRephrase(text, azureLink + 'rephrase')
+            .catch((error) => {
+                console.log(error);
+            })
+            .then(response => {
+                console.log(response);
+            }
+        );
+
+    } else {
+        lastTrigger++;
+    }  
 })
 
 async function sendHttpRequest(method, url, data) {
