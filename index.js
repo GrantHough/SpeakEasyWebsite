5b0671bb1aacddb8ef2312bb14da6dca45a31262
Grant Hough <grant.l.hough@gmail.com>
2023-04-09 22:59:30 -0700

    asdf

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ async function sendHttpRequest(method, url, data) {
         method: method, 
         body: JSON.stringify(data), 
         mode: 'cors',
-        headers: data ? {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://speakeasy.ooo'} : {}
+        headers: data ? {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'http://speakeasy.ooo'} : {}
      }).then(async (response) => {
         return (await response.json());
      }) 
