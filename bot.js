e0ca10f8a2fc7f50fa620abe0ee7c85ff025004a
Grant Hough <grant.l.hough@gmail.com>
2023-09-04 11:14:42 -0700

    asdfasf

--- bot.js
+++ bot.js
@@ -3,23 +3,16 @@ let users = $("#users");
 let servers = $("#servers");
 let sentences = $("#sentences");
 
-const serverLink = "https://speakeasyserver-vkd5vb5lca-uc.a.run.app/";
+const serverLink = "https://speakeasy-367001-default-rtdb.firebaseio.com/speakeasybot/general_data/.json?auth=Rc0ZwbYSkQRIxI0c6QnMZVGnguLxDvy9l710bQy3;"
 
 updateValues();
 
 async function updateValues() {
 
-    fetch(serverLink + "firebase-creds", {
-        mode: 'cors',
-        headers: data ? {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'} : {}
-     }).then(async (response) => {
-        return (await response.json());
-     }).then(async data => {
-        return fetch(data.url).then(results => {
-            return results.json();
-        }).then(stats => {
-            console.log(stats);
-        });
-     });
+    return fetch(serverLink+"bot-stats").then(results => {
+        return results.json();
+    }).then(stats => {
+        console.log(stats);
+    });
 
 }
