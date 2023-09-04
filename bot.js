8caf92b0b66082eb7ad74a6f1e3ca3e4fe68474d
Grant Hough <grant.l.hough@gmail.com>
2023-09-04 11:28:03 -0700

    asdf

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ let users = $("#users");
 let servers = $("#servers");
 let sentences = $("#sentences");
 
-const serverLink = "https://speakeasy-367001-default-rtdb.firebaseio.com/speakeasybot/general_data/.json?auth=Rc0ZwbYSkQRIxI0c6QnMZVGnguLxDvy9l710bQy3;"
+const serverLink = "https://speakeasyserver-vkd5vb5lca-uc.a.run.app/";
 
 updateValues();
 
@@ -13,6 +13,19 @@ async function updateValues() {
         return results.json();
     }).then(stats => {
         console.log(stats);
+        let userCount = addCommas(stats.data.users);
+        let serverCount = addCommas(stats.data.servers);
+        let sentenceCount = addCommas(stats.data.rated);
+
+        users.html(userCount + " users");
+        servers.html(serverCount + " servers");
+        sentences.html(sentenceCount + " sentences rated")
+
     });
 
+}
+
+
+function addCommas(n) {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
